0ba716c00c88a27ec2ca6137037c9e6dfcd3792b
TobyJohnstonUU <johnston-t23@ulster.ac.uk>
2025-11-10 14:15:17 +0000

    Add project files.

---
+++ Week 8 - Inheritance and Abstraction/SmartDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_8___Inheritance_and_Abstraction
+{
+    internal class SmartDevice
+    {
+    }
+}
